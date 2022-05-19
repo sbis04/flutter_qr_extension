c4b6e59e9a88c798981a225df9d56a5fff9d98b8
Souvik Biswas <sbis1999@gmail.com>
2022-05-19 08:26:52 +0530

    Add color list consts

---
+++ lib/color_list.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+const List<Color> qrBackgroundColors = [
+  Colors.white,
+  Colors.orange,
+  Colors.blueGrey,
+  Colors.red,
+  Colors.greenAccent,
+];
+
+const List<Color> qrColors = [
+  Colors.black,
+  Colors.purple,
+  Colors.white,
+  Colors.green,
+  Colors.blue,
+];
